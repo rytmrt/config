e1d9c5ef1f14565dea43020ee58b594910b23e1e
Ryota Morita <ryota.morita.3.8@gmail.com>
2015-12-17 11:45:34 +0900

    setup.shでGitサブモジュールを取得するように修正

--- setup.sh
+++ setup.sh
@@ -7,5 +7,7 @@ ln -s ${abs_path}/zsh/zshrc ~/.zshrc
 ln -s ${abs_path}/zsh/zshenv ~/.zshenv
 
 # VIM
+git submodule init
+git submodule update
 ln -s ${abs_path}/vim ~/.vim
 ln -s ${abs_path}/vim/vimrc ~/.vimrc
