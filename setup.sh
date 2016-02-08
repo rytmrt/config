28bec53de24516396d6595ffced5719ff9dd5bfd
Ryota Morita <ryota.morita.3.8@gmail.com>
2016-02-08 13:03:51 +0900

    フォルダ名変更に合わせてNVIMのシンボリックリンク設定をsetup.shに追加

--- setup.sh
+++ setup.sh
@@ -7,8 +7,10 @@ ln -s ${abs_path}/zsh/zshrc ~/.zshrc
 ln -s ${abs_path}/zsh/zshenv ~/.zshenv
 
 # VIM
-git submodule init
-git submodule update
 ln -s ${abs_path}/vim ~/.vim
 ln -s ${abs_path}/vim/vimrc ~/.vimrc
 ln -s ${abs_path}/vim/gvimrc ~/.gvimrc
+
+# NVIM
+mkdir -p ~/config/nvim/init.vim
+ln -s ${abs_path}/nvim/init.vim ~/config/nvim/init.vim
